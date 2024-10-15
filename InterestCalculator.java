86c25ef5cf9cc02a298a4d984fd337ac14ebea7e
Rambabu Pachipala <147068166+Rambabu28@users.noreply.github.com>
2024-10-15 17:12:37 +0530

    Add files via upload

--- InterestCalculator.java
+++ InterestCalculator.java
@@ -1,52 +1,49 @@
 package coreJava.ExceptionHandlingPractice;
 
-
 import java.util.Scanner;
+
 /*---------------------------------------------------------------*/
 /*----- Custom exception class for principal ------*/
-class PrincipalException extends Exception
-{
+class PrincipalException extends Exception {
 	/*---- constructor ----*/
 	public PrincipalException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
-	}	
-	
+	}
+
 }
+
 /*--------------------------------------------------------------*/
 /*------ Custom exception class for rate ----*/
-class RateException extends Exception
-{
+class RateException extends Exception {
 	/*----- Constructor -----*/
-	public RateException(String msg)
-	{
+	public RateException(String msg) {
 		super(msg);
 	}
 }
+
 /*--------------------------------------------------------------*/
 /*------ Custom exception class for Time ----*/
-class TimeException extends Exception
-{
+class TimeException extends Exception {
 	/*---- constructor ----*/
-	public TimeException(String msg)
-	{
+	public TimeException(String msg) {
 		super(msg);
 	}
 }
+
 /*--------------------------------------------------------------*/
 /*------ Defining Interest class ----*/
-class Interest
-{
+class Interest {
 	/*---- Member variables ----*/
 	float principal;
 	float rate;
 	int time;
+
 	/*----- Constructor -----*/
-	public Interest()
-	{
+	public Interest() {
 		/*----- Scanner class object ----*/
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter principal in time : ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter principal in Rupees : ");
 		principal = sc.nextFloat();
 		System.out.print("Enter rate(in %) : ");
 		rate = sc.nextFloat();
@@ -54,53 +51,46 @@ class Interest
 		time = sc.nextInt();
 		System.out.println("-----------------------------------------------------");
 	}
+
 	/*------ Method to display data -----*/
-	private void displayInfo()
-	{
-		System.out.println("Principal : Rs "+principal);
-		System.out.println("Rate : "+rate+" %");
-		System.out.println("Time : "+time+" year");
+	private void displayInfo() {
+		System.out.println("Principal : Rs " + principal);
+		System.out.println("Rate : " + rate + " %");
+		System.out.println("Time : " + time + " year");
 	}
+
 	/*---------------------------------------------------------------------*/
 	/*---- method to calculate simple interest -----*/
-	public void calculateSimpleInterest()
-	{
-		try
-		{
-			if(principal<0)
-			{
-				throw new PrincipalException("Principal cannot be negative");
-			}
-			else if(rate < 0)
-			{
-				throw new RateException("Rate cannot be negative");
-			}
-			else if(time < 0)
-			{
-				throw new TimeException("Time cannot be negative");
-			}
-			else
-			{
-				float si = (principal * rate * time)/100;
-				/*---- displaying data ----*/
-				displayInfo();
-				System.out.println("Simple Interest : Rs "+si);
-			}
-		}
-		catch(PrincipalException | RateException | TimeException exceptionObj)
-		{
-			System.err.println(exceptionObj.getMessage());
+	public void calculateSimpleInterest() throws PrincipalException, RateException, TimeException {
+
+		if (principal < 0) {
+			throw new PrincipalException("Principal cannot be negative");
+		} else if (rate < 0) {
+			throw new RateException("Rate cannot be negative");
+		} else if (time < 0) {
+			throw new TimeException("Time cannot be negative");
+		} else {
+			float si = (principal * rate * time) / 100;
+			/*----to display data ----*/
+			displayInfo();
+			System.out.println("Simple Interest : Rs " + si);
 		}
 	}
 }
+
 /*--------------------------------------------------------------*/
+/* ...................... main class.............. */
 public class InterestCalculator {
 
 	public static void main(String[] args) {
-		/*---- Object of Interest --*/
-		Interest si=new Interest();
-		si.calculateSimpleInterest();
+		/*---- Object of Interest class --*/
+		Interest si = new Interest();
+		try {
+			/* calling simple intrest method */
+			si.calculateSimpleInterest();
+		} catch (PrincipalException | RateException | TimeException exceptionObj) {
+			System.err.println(exceptionObj.getMessage());
+		}
 	}
 
 }
-
